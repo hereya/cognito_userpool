aea7af88abdc18edfba5db552be742debfae227f
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-10-05 21:49:28 +0200

    add user pool name input parameter

--- lib/cognito_userpool-stack.ts
+++ lib/cognito_userpool-stack.ts
@@ -1,21 +1,27 @@
-import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, CfnParameter, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { UserPool } from 'aws-cdk-lib/aws-cognito';
+
 
 export class CognitoUserPoolStack extends Stack {
     public readonly userPoolId: string;
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        const userPoolNameParam = new CfnParameter(this, 'userPoolName', {
+            type: 'String',
+            description: 'The name of the user pool',
+        });
+
         const pool = new UserPool(this, 'UserPool', {
             removalPolicy: RemovalPolicy.DESTROY,
+            userPoolName: userPoolNameParam.valueAsString,
         });
         this.userPoolId = pool.userPoolId;
 
         new CfnOutput(this, 'userPoolId', {
             value: pool.userPoolId,
             description: 'The ID of the user pool',
-            exportName: 'userPoolId'
         });
     }
 }
